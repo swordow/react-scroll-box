4f96c7831cbafbd80f0df4cbf5517a89a7c9102a
Savva Mikhalevski <smikhalevski@gmail.com>
2016-04-28 13:21:39 +0300

    `ScrollBox` is not a functional component for `ref` support

--- src/main/ScrollBox.js
+++ src/main/ScrollBox.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {GenericScrollBox} from './GenericScrollBox';
 
-export function ScrollBox(props) {
-  return (
-    <GenericScrollBox {...props}>
-      <div className="scroll-box__viewport">
-        {props.children}
-      </div>
-    </GenericScrollBox>
-  );
+export class ScrollBox extends React.Component {
+
+  render() {
+    return (
+      <GenericScrollBox {...this.props}>
+        <div className="scroll-box__viewport">
+          {this.props.children}
+        </div>
+      </GenericScrollBox>
+    );
+  }
 }

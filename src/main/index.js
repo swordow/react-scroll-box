e9d3b67e9c6de837802ca4548c9250b099d26357
Savva Mikhalevski <smikhalevski@gmail.com>
2016-05-12 13:53:28 +0300

    Compile styles apart from JS code

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,2 @@
-import './scroll-box.less';
-
 export {ScrollBox} from './ScrollBox';
 export {GenericScrollBox, FastTrack, ScrollAxes} from './GenericScrollBox';
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: './src/main/index.js'
+    index: [
+      './src/main/index.js',
+      './src/main/index.less'
+    ]
   },
   output: {
     path: './target/out',
